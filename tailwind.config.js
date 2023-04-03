917aa689057b0504322ee1da41fc87d1e7895928
ShayanTheNerd <sh.zamani384@gmail.com>
2023-04-03 23:25:03 +0330

    Migrated to Tailwind CSS v3.3 & added Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-module.exports = {
-  content: ["*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './main.js'],
+	theme: {
+		extend: {
+			screens: {
+				support: { raw: '(hover: hover)' }, // check if hover is supported on the device
+			},
+		},
+	},
+};
