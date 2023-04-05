65e79b5df3f032ecb924be9de6a90ab610205f66
ShayanTheNerd <sh.zamani384@gmail.com>
2023-04-06 01:46:38 +0330

    Full code refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './main.js'],
+	content: ['./index.html', './assets/js/main.js'],
 	theme: {
 		extend: {
 			screens: {
