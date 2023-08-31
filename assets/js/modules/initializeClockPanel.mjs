07c9c27a0c05890576f81087d18c4f7cd6264ba8
Shayan Zamani <sh.zamani384@gmail.com>
2023-09-01 00:01:25 +0430

    Complete code refactor

--- assets/js/modules/initializeClockPanel.mjs
+++ assets/js/modules/initializeClockPanel.mjs
@@ -1,8 +1,18 @@
-export function updateClockPanel(clockNodes = {}, datetimeNodes = {}) {
-	const { secondHand, minuteHand, hourHand } = clockNodes;
-	const { timeContainer, dateContainer } = datetimeNodes;
+export default function initializeClockPanel() {
+	const hourHand = document.querySelector('#hourHand');
+	const minuteHand = document.querySelector('#minuteHand');
+	const secondHand = document.querySelector('#secondHand');
+	const timeContainer = document.querySelector('#currentTime');
+	const dateContainer = document.querySelector('#currentDate');
 
-	// update current date and time every second
+	updateClockPanel({ secondHand, minuteHand, hourHand }, { timeContainer, dateContainer });
+}
+
+function updateClockPanel(clockNodesObject, datetimeNodesObject) {
+	const { timeContainer, dateContainer } = datetimeNodesObject;
+	const { secondHand, minuteHand, hourHand } = clockNodesObject;
+
+	// Update the current date and time every second
 	setInterval(() => {
 		const now = new Date();
 		const seconds = now.getSeconds();
@@ -19,8 +29,8 @@ export function updateClockPanel(clockNodes = {}, datetimeNodes = {}) {
 	}, 1000);
 }
 
-function updateAnalogClock(time = {}, secondHand, minuteHand, hourHand) {
-	const { seconds, minutes, hours } = time;
+function updateAnalogClock(timeComponents, secondHand, minuteHand, hourHand) {
+	const { seconds, minutes, hours } = timeComponents;
 	const secondsDegrees = (seconds / 60) * 360 + 90;
 	const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
 	const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
@@ -30,24 +40,20 @@ function updateAnalogClock(time = {}, secondHand, minuteHand, hourHand) {
 	hourHand.style.rotate = `${hoursDegrees}deg`;
 }
 
-function displayDateAndTime(time = {}, timeContainer, dateContainer) {
+function displayDateAndTime(timeComponents, timeContainer, dateContainer) {
 	/* prettier-ignore */
 	const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 	const weekdays = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-	const { seconds, minutes, hours, dayNumber, date, monthNumber, year } = time;
+	const { seconds, minutes, hours, dayNumber, date, monthNumber, year } = timeComponents;
 	const today = weekdays[dayNumber + 1];
 	const month = months[monthNumber];
 
-	// formatted time components
+	// Format time components
 	const [sec, min, hour, dateString, monthString] = [seconds, minutes, hours, date, monthNumber].map(item =>
 		item.toString().padStart(2, '0'),
 	);
 
-	// display current time
 	timeContainer.textContent = `${hour} : ${min} : ${sec}`;
-	timeContainer.setAttribute('datetime', `${hour}:${min}:${sec}`);
-
-	// display current date
 	dateContainer.textContent = `${today}, ${month} ${date} ${year}`;
-	dateContainer.setAttribute('datetime', `${year}-${monthString}-${dateString}T${hour}:${min}:${sec}`);
+	time_element.setAttribute('datetime', `${year}-${monthString}-${dateString}T${hour}:${min}:${sec}`);
 }
